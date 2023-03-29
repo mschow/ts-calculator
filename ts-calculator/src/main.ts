6471c927087d372a746bf49c3822a9a5e6398960
micah.schow@gmail.com <micah.schow@gmail.com>
2023-03-28 22:24:00 -0600

    Delegate events for calculator buttons.

--- ts-calculator/src/main.ts
+++ ts-calculator/src/main.ts
@@ -1,3 +1,35 @@
 import './style.css'
+import { Operations } from './operations.enum'
 
-document.querySelector<HTMLDivElement>('#app')
+function initializeCalculator(){
+  const calculatorButtons: HTMLElement | null = document.getElementById('calc-buttons');
+  
+  calculatorButtons?.addEventListener('click', (event)=>{
+    const element: HTMLElement = <HTMLElement> event.target;
+    switch(<Operations> element.dataset['operation']){
+      case Operations.ADD:
+        console.log("Add");
+        break;
+      case Operations.SUBTRACT:
+        console.log("Subtract");
+        break;
+      case Operations.MULTIPLY:
+        console.log("Multiply");
+        break;
+      case Operations.DIVIDE:
+        console.log("Divide");
+        break;
+      case Operations.CLEAR:
+        console.log("Clear");
+        break;
+      case Operations.DELETE:
+        console.log("Delete");
+        break;
+      case Operations.APPEND_NUMBER:
+        console.log("Append");
+        break;
+    }
+  })
+}
+
+initializeCalculator();
