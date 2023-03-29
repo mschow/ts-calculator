d9ed26af6f0a85cf59df7b55049e5400d31b92ed
micah.schow@gmail.com <micah.schow@gmail.com>
2023-03-28 23:23:15 -0600

    Refactor main.ts to be more object-oriented.

--- ts-calculator/src/main.ts
+++ ts-calculator/src/main.ts
@@ -2,72 +2,74 @@ import './style.css'
 import { Operations } from './operations.enum'
 import { CalculatorDom } from './calculator-dom.model';
 
-let calculatorDOM: CalculatorDom = {
-  buttonContainer: <HTMLElement> document.getElementById('calc-buttons'),
-  currentOperand: <HTMLElement> document.querySelector('.current-operand'),
-  previousOperand: <HTMLElement> document.querySelector('.previous-operand'),
+export class Main {
+  private readonly calculatorDOM: CalculatorDom = {
+    buttonContainer: <HTMLElement> document.getElementById('calc-buttons'),
+    currentOperand: <HTMLElement> document.querySelector('.current-operand'),
+    previousOperand: <HTMLElement> document.querySelector('.previous-operand'),
+  }
+
+  constructor(){
+    this.assignEventListeners();
+  }
+
+  private assignEventListeners(): void{
+    this.calculatorDOM.buttonContainer?.addEventListener('click', (event)=>{
+      const element: HTMLElement = <HTMLElement> event.target;
+      switch(<Operations> element.dataset['operation']){
+        case Operations.ADD:
+          this.calcAdd(element.innerText);
+          break;
+        case Operations.SUBTRACT:
+          this.calcSubtract(element.innerText);
+          break;
+        case Operations.MULTIPLY:
+          this.calcMultiply(element.innerText);
+          break;
+        case Operations.DIVIDE:
+          this.calcDivide(element.innerText);
+          break;
+        case Operations.CLEAR:
+          this.calcClear();
+          break;
+        case Operations.DELETE:
+          this.calcDelete();
+          break;
+        case Operations.APPEND_NUMBER:
+          this.calcAppend(element.innerText);
+          break;
+      }
+    })
+  }
+  
+  private calcAdd(value: string): void {}
+  
+  private calcSubtract(value: string): void {}
+  
+  private calcMultiply(value: string): void {}
+  
+  private calcDivide(value: string): void {}
+  
+  private calcClear(): void {}
+  
+  private calcDelete(): void {}
+  
+  private calcAppend(value: string): void {
+    this.assignCurrentOperand(value);
+  }
+  
+  private getNumberFromCurrentOperand():number{
+    return parseInt(this.calculatorDOM.currentOperand.innerText);
+  }
+  
+  private assignCurrentOperand(value: string): void{
+    this.calculatorDOM.currentOperand.innerText = value;
+  };
+  
+  private setCurrentOperandToPrevious(endingCharacter: '+' | '-' | '%' | '*' | '' = ''): void {
+    // const currentOperandElem:HTMLElement = <HTMLElement> document.querySelector('.current-operand');
+    // const previousOperand
+  }
 }
 
-function initializeCalculator(): void{
-  assignEventListeners();
-}
-
-function assignEventListeners(): void{
-  calculatorDOM.buttonContainer?.addEventListener('click', (event)=>{
-    const element: HTMLElement = <HTMLElement> event.target;
-    switch(<Operations> element.dataset['operation']){
-      case Operations.ADD:
-        calcAdd(element.innerText);
-        break;
-      case Operations.SUBTRACT:
-        calcSubtract(element.innerText);
-        break;
-      case Operations.MULTIPLY:
-        calcMultiply(element.innerText);
-        break;
-      case Operations.DIVIDE:
-        calcDivide(element.innerText);
-        break;
-      case Operations.CLEAR:
-        calcClear();
-        break;
-      case Operations.DELETE:
-        calcDelete();
-        break;
-      case Operations.APPEND_NUMBER:
-        calcAppend(element.innerText);
-        break;
-    }
-  })
-}
-
-function calcAdd(value: string): void {}
-
-function calcSubtract(value: string): void {}
-
-function calcMultiply(value: string): void {}
-
-function calcDivide(value: string): void {}
-
-function calcClear(): void {}
-
-function calcDelete(): void {}
-
-function calcAppend(value: string): void {
-  assignCurrentOperand(value);
-}
-
-function getNumberFromCurrentOperand():number{
-  return parseInt(calculatorDOM.currentOperand.innerText);
-}
-
-function assignCurrentOperand(value: string): void{
-  calculatorDOM.currentOperand.innerText = value;
-};
-
-function setCurrentOperandToPrevious(endingCharacter: '+' | '-' | '%' | '*' | '' = ''): void {
-  // const currentOperandElem:HTMLElement = <HTMLElement> document.querySelector('.current-operand');
-  // const previousOperand
-}
-
-initializeCalculator();
+new Main();
