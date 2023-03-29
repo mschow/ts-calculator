af87e56f6ea58562a3ae900d71422f71c0239e55
micah.schow@gmail.com <micah.schow@gmail.com>
2023-03-28 22:25:34 -0600

    Delegate events for calculator buttons.

---
+++ ts-calculator/src/operations.enum.ts
@@ -0,0 +1,9 @@
+export enum Operations{
+    CLEAR = 'CLEAR',
+    DELETE = 'DELETE',
+    MULTIPLY = 'MULTIPLY',
+    DIVIDE = 'DIVIDE',
+    ADD = 'ADD',
+    SUBTRACT = 'SUBTRACT',
+    APPEND_NUMBER = 'APPEND_NUMBER',
+}
